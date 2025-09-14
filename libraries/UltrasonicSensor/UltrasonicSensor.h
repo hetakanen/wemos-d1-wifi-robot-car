00364d3bd344aa812b84e5da38adf8f34af22746
Hetakanen <86298799+hetakanen@users.noreply.github.com>
2025-09-14 21:24:29 +0300

    feat: v0.5

--- libraries/UltrasonicSensor/UltrasonicSensor.h
+++ libraries/UltrasonicSensor/UltrasonicSensor.h
@@ -6,14 +6,14 @@ class UltrasonicSensor {
             : ECHO(echoPin), TRIGGER(triggerPin), SERVO(servoPin) {}
         void setup();
         float readDistance();
+        int getRotation();
+        void lookAt(int value);
+        const int LEFT = 2200;
+        const int RIGHT = 1200;
+        const int CENTER = 1750;
     private:
         const int ECHO;
         const int TRIGGER;
         const int SERVO;
-
-        const int LEFT = 2200;
-        const int RIGHT = 1200;
-        const int CENTER = 1750;
-
         Servo myservo;
 };
