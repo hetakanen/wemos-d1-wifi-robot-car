8af9615152f9b86d767b5ee6d3a5b6e8ae893a19
Hetakanen <86298799+hetakanen@users.noreply.github.com>
2025-09-18 21:39:41 +0300

    feat: Add led testers

---
+++ led-blink-tester/led-blink-tester.ino
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * LED Blink Tester
+ * 
+ * Program tests LED blink by making an LED blink on and off at 1-second intervals.
+ *
+ ******************************************************************************/
+
+const int LED = D12;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+ void loop() {
+  analogWrite(LED, 255);
+  delay(1000);
+  analogWrite(LED, 0);
+  delay(1000);
+}
