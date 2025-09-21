0022a1e080dfe47bdc78861acf7ecc4b1f45a43e
Hetakanen <86298799+hetakanen@users.noreply.github.com>
2025-09-21 11:54:35 +0300

    feat: Add led library

---
+++ libraries/Led/Led.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "led.h"
+
+void Led::setup() {      
+    pinMode(PIN, OUTPUT);
+}
+
+void Led::toggle() {
+    analogWrite(PIN, isLedOn ? LED_OFF : LED_ON);
+    isLedOn = !isLedOn;
+}
+
+void Led::on() {
+    analogWrite(PIN, LED_ON);
+    isLedOn = true;
+}
+
+void Led::off() {
+    analogWrite(PIN, LED_OFF);
+    isLedOn = false;
+}
